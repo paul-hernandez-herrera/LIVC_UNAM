4ac02cb4ee2678f759363740e4add6604693e1b0
Paul Hernandez <paul.hernandez@ibt.unam.mx>
2024-01-26 10:13:56 -0600

    refactoring: swc_to_vtk_lines

--- python_code/util/util_vtk.py
+++ python_code/util/util_vtk.py
@@ -1,29 +1,24 @@
 from pathlib import Path
 import numpy as np
 
-def swc_to_vtk_lines(folder_path, file_name, swc):
-    
-    #just to make sure that we have a numpy array
+def swc_to_vtk_lines(file_output, swc):
     swc = np.array(swc)
     
-    points = swc[:,2:5]
+    points, conections = swc[:,2:5], swc[:,[0,6]]
     
-    conections = swc[:,[0,6]]
-    
-    conections = conections[conections[:,1]>-1,:] 
-    conections = conections -1
+    conections = conections[conections[:,1]>-1,:] -1
     conections = np.hstack((2*np.ones((conections.shape[0],1)),conections))
+
+    file_output = Path(file_output)
+    file_output = file_output if file_output.suffix == ".vtk" else file_output.with_suffix('.vtk')
     
-    f = open(Path(folder_path) / (file_name + '.vtk'), 'w')
-    f.write('# vtk DataFile Version 2.0\n\n')
-    f.write('ASCII\n\n')
-    
-    f.write('DATASET POLYDATA\n')
-    f.write('POINTS {} float\n'.format(points.shape[0]))
-    np.savetxt(f, points, fmt="%4.5f")
-    f.write('LINES {:d} {:d}\n'.format(conections.shape[0],3*conections.shape[0]))
-    np.savetxt(f, conections, fmt="%i")
-    f.close()
+    with open(file_output, 'w') as f:
+        f.write('# vtk DataFile Version 2.0\n\nASCII\n\n')
+        f.write('DATASET POLYDATA\n')
+        f.write('POINTS {} float\n'.format(points.shape[0]))
+        np.savetxt(f, points, fmt="%4.5f")
+        f.write('LINES {:d} {:d}\n'.format(conections.shape[0],3*conections.shape[0]))
+        np.savetxt(f, conections, fmt="%i")
     
 def points_to_vectors(folder_path, file_name, positions, vectors):
     
