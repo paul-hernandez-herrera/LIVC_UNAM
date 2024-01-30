3f51fdd08511c7ebdb565a607594ecd8cfad6a7e
Paul Hernandez <paul.hernandez@ibt.unam.mx>
2024-01-29 20:58:54 -0600

    updating file names

--- python_code/sperm/swc_to_microns_txt_file.py
+++ python_code/sperm/swc_to_microns_txt_file.py
@@ -6,10 +6,10 @@ from util.util_swc import swc_to_micron
 from util.basic_func import create_file_in_case_not_exist
 from pathlib import Path
 
-folder_path_height_txt = r"C:\Users\jalip\Documentos\Proyectos\Sperm\Campo_claro\HIGH_VISCOCITY\2017_11_24_HIGH_VISCOCITY_DONE\Exp11_stacks"
-folder_path_swc = r"C:\Users\jalip\Documentos\Proyectos\Sperm\Campo_claro\HIGH_VISCOCITY\2017_11_24_HIGH_VISCOCITY_DONE\Exp11_stacks\trace_head_removed\trace_voxels"
-folder_output = r"C:\Users\jalip\Documentos\Proyectos\Sperm\Campo_claro\HIGH_VISCOCITY\2017_11_24_HIGH_VISCOCITY_DONE\Exp11_stacks\trace_head_removed\trace_micras"
-stack_id = "Exp11_stacks"
+folder_path_height_txt = r"C:\Users\jalip\Documentos\Proyectos\Sperm\Campo_claro\HIGH_VISCOCITY\2017_11_09_HIGH_VISCOCITY_DONE\Exp8_stacks"
+folder_path_swc = r"C:\Users\jalip\Documentos\Proyectos\Sperm\Campo_claro\HIGH_VISCOCITY\2017_11_09_HIGH_VISCOCITY_DONE\Exp8_stacks\trace_head_removed\trace_voxels"
+folder_output = r"C:\Users\jalip\Documentos\Proyectos\Sperm\Campo_claro\HIGH_VISCOCITY\2017_11_09_HIGH_VISCOCITY_DONE\Exp8_stacks\trace_head_removed\trace_micras"
+stack_id = "Exp8_stacks"
 
 create_file_in_case_not_exist(folder_output)
 
